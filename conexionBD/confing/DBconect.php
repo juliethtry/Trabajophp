2d29753a382c2c284aa8731a814e93b34580d6e7
Juliethtry <julieth_baquero@ispaeducacion.edu.co>
2021-06-09 15:33:01 -0500

    conexion

---
+++ conexionBD/confing/DBconect.php
@@ -0,0 +1,48 @@
+<?php
+
+    class Database {
+
+        public $db; //controladores db
+        private static $dns = "mysql:host=localhost;dbname=prueba"; //url de la base de datos
+        private static $user = "root"; //usuario de la conexión
+        private static $pass = ""; //contraseña del usuario
+        private static $instance; //instrancia de la conexion
+
+        public function __construct(){
+            $this->db = new PDO(self::$dns, self::$user, self::$pass);
+        }
+
+        public static function getInstance(){
+            if(!isset(self::$instance)){
+                $object = __CLASS__;
+                self::$instance = new $object;
+            }
+            return self::$instance;
+        }
+
+        public function insertar($nombre, $apellido, $edad, $email){
+
+            try {
+                $conexion = Database::getInstance(); //obtiene la instancia de la clase
+                $query = $conexion->db->prepare("INSERT INTO persona (nombre, apellido, email, edad) VALUES (:nombre, :apellido, :email, :edad)");
+                $query->execute(
+                    array(
+                        ':nombre' => $nombre,
+                        ':apellido' => $apellido,
+                        ':email' => $email,
+                        ':edad' => $edad
+                    )
+                );
+
+                return 1; //retorna 1 si fue exitoso
+
+            } catch(PDOException $error){
+                return 0; // retporna o si falla
+            }
+
+        }
+
+
+    }
+
+?>
